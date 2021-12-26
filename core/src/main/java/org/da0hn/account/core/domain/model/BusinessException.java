5844fd22253b3b57b0d0e80a4415ca1aa02320ff
Gabriel Honda <gabrielhondacba@gmail.com>
2021-12-26 10:05:31 -0400

    fix: add attribute `serialVersionUID`

--- core/src/main/java/org/da0hn/account/core/domain/model/BusinessException.java
+++ core/src/main/java/org/da0hn/account/core/domain/model/BusinessException.java
@@ -1,6 +1,10 @@
 package org.da0hn.account.core.domain.model;
 
+import java.io.Serial;
+
 public class BusinessException extends RuntimeException {
+
+  @Serial private static final long serialVersionUID = -6928613625685489378L;
 
   public BusinessException(final String message) {
     super(message);
