b4374d635eef1d635de2141f5c31b1eb1f2cf89d
Gabriel Honda <gabrielhondacba@gmail.com>
2021-12-26 10:10:58 -0400

    feat: add error messages

---
+++ core/src/main/java/org/da0hn/account/core/domain/model/ErrorMessage.java
@@ -0,0 +1,23 @@
+package org.da0hn.account.core.domain.model;
+
+public final class ErrorMessage {
+
+  private ErrorMessage() {
+  }
+
+  public static void mandatory(final String name) {
+    throw new BusinessException(name + " é obrigatório.");
+  }
+
+  public static void noexistent(final String name) {
+    throw new BusinessException(name + " é inexistente.");
+  }
+
+  public static void insufficientFunds() {
+    throw new BusinessException("Saldo insuficiente");
+  }
+
+  public static void sameAccount() {
+    throw new BusinessException("Conta débito e crédito devem ser diferentes.");
+  }
+}
