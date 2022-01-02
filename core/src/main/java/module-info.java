399cfb1924d5314eb676c1e3b7a170e4e987c305
Gabriel Honda <gabrielhondacba@gmail.com>
2022-01-02 10:59:31 -0400

    feat: exports and open for reflection classes

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,5 +1,17 @@
 module account.core {
   requires javax.inject;
   requires spring.tx;
+
+  exports org.da0hn.account.adapters;
+  exports org.da0hn.account.core.usecases.impl;
+  exports org.da0hn.account.core.usecases.ports;
+  exports org.da0hn.account.core.domain.model;
+  exports org.da0hn.account.core.domain.service;
+
+  opens org.da0hn.account.adapters;
+  opens org.da0hn.account.core.usecases.impl;
+  opens org.da0hn.account.core.usecases.ports;
+  opens org.da0hn.account.core.domain.service;
+
 }
 
