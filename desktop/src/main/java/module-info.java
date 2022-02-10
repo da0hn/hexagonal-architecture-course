f63270e77e24e3437aab9c91549e3c16fbfede4c
da0hn <gabrielhondacba@gmail.com>
2022-02-10 17:18:24 -0400

    feat: add `desktop` module

---
+++ desktop/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module account.desktop {
+
+  requires account.core;
+
+  requires javax.inject;
+  requires spring.tx;
+  requires spring.core;
+  requires spring.beans;
+  requires spring.context;
+
+  requires javafx.controls;
+
+}
